415810a90eb39ed91837c09aeef5e97d59a8ba8d
suvaranjan <suvaranjan01@gmail.com>
2024-05-10 08:53:29 +0530

    changes in server.js

--- server/server.js
+++ server/server.js
@@ -8,13 +8,10 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const corsOptions = {
-    origin: 'https://event-management-by-suvaranjan.vercel.app/',
-};
 
 // Middleware
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan("tiny"));
 
 // Routes
